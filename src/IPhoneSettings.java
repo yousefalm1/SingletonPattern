369d053235244c1b91223a07e39796b2fbfbcb8c
Yousef Almesaeed <yousefalm@Outlook.com>
2024-11-05 13:46:47 +0300

    finished task

--- src/IPhoneSettings.java
+++ src/IPhoneSettings.java
@@ -1,4 +1,54 @@
-package PACKAGE_NAME;
-
 public class IPhoneSettings {
+
+    private static IPhoneSettings instance;
+
+    private boolean wifi;
+    private int brightness;
+
+    private IPhoneSettings() {
+        wifi = true;
+        brightness = 80;
+    }
+
+
+    public static IPhoneSettings getInstance() {
+        if (instance == null) {
+            instance = new IPhoneSettings();
+        }
+        return instance;
+    }
+
+
+    public boolean isWifi() {
+        return wifi;
+    }
+
+    public void enableWifi() {
+        wifi = true;
+    }
+
+    public void disableWifi() {
+        wifi = false;
+    }
+
+
+    public int getBrightness() {
+        return brightness;
+    }
+
+    public void setBrightness(int brightness) {
+        if (brightness >= 0 && brightness <= 100) {
+            this.brightness = brightness;
+
+        }
+
+    }
+
+    @Override
+    public String toString() {
+        return "IPhoneSettings{" +
+                "wifi=" + wifi +
+                ", brightness=" + brightness +
+                '}';
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,17 @@
-package PACKAGE_NAME;
-
 public class Main {
+
+    public static void main(String[] args) {
+
+        IPhoneSettings iPhoneSettings = IPhoneSettings.getInstance();
+
+
+        iPhoneSettings.setBrightness(90);
+        iPhoneSettings.enableWifi();
+        iPhoneSettings.disableWifi();
+
+
+        System.out.println(iPhoneSettings.toString());
+
+    }
+
 }
